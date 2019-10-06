1b33b5c22b848d7a83197a323590f0c7c5cb3728
Patrick Armentrout <patrick.armentrout@gmail.com>
2019-10-06 14:03:30 -0600

    Initial form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dice-game';
+
+  settingUp: boolean = true;
+  model: StartGameForm = new StartGameForm('','',10000,'','','','');
+
+  startNewGame() {
+    this.settingUp = true;
+  }
+
+  finishSetup() {
+    this.settingUp = false;
+  }
+}
+
+export class StartGameForm {
+
+  constructor(
+    public player1: string,
+    public player2: string,
+    public totalScore: number,
+    public player3?: string,
+    public player4?: string,
+    public player5?: string,
+    public player6?: string
+  ) {  this.totalScore = 10000; }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
